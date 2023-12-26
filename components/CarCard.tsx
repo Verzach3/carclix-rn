374ac639bab21404ef4334a72e8bcde44db7a7bb
Gabriel Correa <gabrielcorreacardenas@gmail.com>
2023-12-26 15:33:27 -0500

    chore: update car screen

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,11 +1,20 @@
 import { View, Text, ScrollView } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Card } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
 import { Vehicle } from "../Types/vehicle";
-
+import { getImagesById } from "../services/vehicles";
+import { SERVER_URL } from "../constants";
 const CarCard = ({ vehicle }: { vehicle: Vehicle }) => {
   const navigation = useNavigation<any>();
+  const [vehicleImageId, setVehicleImageId] = useState("");
+  useEffect(() => {
+    const getVehicleImage = async () => {
+      const data = await getImagesById(vehicle.id);
+      setVehicleImageId(data.data[0].id);
+    };
+    getVehicleImage();
+  });
   return (
     <View>
       <Card
@@ -14,12 +23,19 @@ const CarCard = ({ vehicle }: { vehicle: Vehicle }) => {
       >
         <Card.Cover
           style={{ borderRadius: 5 }}
-          source={{ uri: "https://picsum.photos/700" }}
+          source={{
+            uri: `${SERVER_URL}/vehicles/images/one/${vehicleImageId}`,
+          }}
         />
-        <Card.Title title={`${vehicle.vehicle_maker} ${vehicle.vehicle_model}`} subtitle={vehicle.vehicle_maker} />
-        <Card.Content>
-          <Text>$ {vehicle.purchase_price}</Text>
-        </Card.Content>
+        <Card.Title
+          title={`${Intl.NumberFormat("es-CO", {
+            style: "currency",
+            currency: "COP",
+          }).format(Number(vehicle.purchase_price))}`}
+          titleVariant="titleLarge"
+        />
+        {/* <Card.Title title={`${vehicle.vehicle_maker} ${vehicle.vehicle_model}`} subtitle={vehicle.vehicle_maker} /> */}
+        {/* <Text>$ {vehicle.purchase_price}</Text> */}
         <Card.Actions>
           <Button
             mode="contained"
--- services/vehicles.ts
+++ services/vehicles.ts
@@ -4,3 +4,8 @@ export async function getVehicles() {
     const res = await axios.get(SERVER_URL + "/vehicles/all")
     return res
 }
+
+export async function getImagesById(id: number) {
+    const res = await axios.get(SERVER_URL + `/vehicles/images/list/${id}`)
+    return res
+}
